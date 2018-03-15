1a0d47d85e143f5f00c1d7d05c7a1aa202bd7869
David Fisher <fisherds@rose-hulman.edu>
2018-03-15 12:20:46 -0400

    New Game button for both iPhone and iPad

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -15,9 +15,8 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  @IBAction func pressedNewGame(_ sender: Any) {
+    print("You pressed new game!")
   }
 
 
